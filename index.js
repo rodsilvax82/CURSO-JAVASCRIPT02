13e3cb7fe6991d5c7581c2e19de34cf16cbf195c
rodsilvax822 <rodrigocarvalho.pax@gmail.com>
2021-01-17 03:51:43 -0300

    Return no metodo

--- index.js
+++ index.js
@@ -5,69 +5,34 @@ class Cliente {
 
 class ContaCorrente {
     Agencia;
-    Saldo;
-<<<<<<< HEAD
+    _Saldo = 0;
     // #Saldo = 0 ---- Deixar a variável privada
-=======
->>>>>>> 359f5ac67eddda1f7d409bf9fc7a4ccce9275761
 
     sacar(valor) {
-        if (this.Saldo >= valor) {
-            this.Saldo -= valor;
+        if (this._Saldo >= valor) {
+            this._Saldo -= valor;
+            return valor;
         }
+        return
     }
-<<<<<<< HEAD
     depositar(valor) {
         if (valor > 0) {
-            this.Saldo += valor;
+            this._Saldo += valor;
         }
     }
 }
 
-=======
-}
-
-
-
-
-
-
->>>>>>> 359f5ac67eddda1f7d409bf9fc7a4ccce9275761
 const cliente1 = new Cliente();
 const conta1 = new ContaCorrente();
-let valorSacado = 200;
-
 cliente1.Nome = "Rodrigo";
 cliente1.CPF = 35672009840;
 conta1.Agencia = 1001;
 
-conta1.Saldo = 0;
-console.log(conta1.Saldo);
-<<<<<<< HEAD
 conta1.depositar(100);
-console.log(conta1.Saldo);
 conta1.depositar(100);
-console.log(conta1.Saldo);
 conta1.depositar(100);
-console.log(conta1.Saldo);
-conta1.sacar(50);
-=======
-conta1.Saldo = 100;
-console.log(conta1.Saldo);
-conta1.Saldo = 50;
->>>>>>> 359f5ac67eddda1f7d409bf9fc7a4ccce9275761
-console.log(conta1.Saldo);
+const valorSacado = conta1.sacar(50);
 
-const cliente2 = new Cliente();
-const conta2 = new ContaCorrente();
-cliente2.Nome = "Tatiana";
-cliente2.CPF = 15935725840;
-conta2.Agencia = 1001;
-conta2.Saldo = 0;
+console.log(valorSacado);
+console.log(conta1)
 
-<<<<<<< HEAD
-console.log(conta1);
-=======
-// console.log(cliente1);
->>>>>>> 359f5ac67eddda1f7d409bf9fc7a4ccce9275761
-// console.log(cliente2);
